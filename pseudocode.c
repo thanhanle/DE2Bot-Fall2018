e3bb401d6c32ae7dc89129ed93d9428bab319a13
Avidwindsurfer <avidwindsurfer@gmail.com>
2018-11-12 20:59:42 -0500

    yall like it

--- pseudocode.c
+++ pseudocode.c
@@ -1,6 +1,6 @@
 void groundZero(void){
 for(int i = 0; i<360; i++){
-	if(distance(i)>5000){
+	if(distance(i)>7000){
 		// increment zero counter
 		zeroCounter++;
 	}
@@ -12,12 +12,28 @@ for(int i = 0; i<360; i++){
 	// I think this means that we've had more than 3 zeros in a row, but you guys can fiddle with this number. Maybe put it on the 
 	// sevenseg display
 	// Start looking for the right reflector
-	lookForReflector();
+	foundReflector(i);
 	}
 }
 }
 
 
-void lookForReflector(void){
-	// our first data point should be near our 
+void foundReflector(int currentAngle){
+	// our first data point should be near the others
+	while(currentAngle<7000){
+		// we're still in the reflector
+		// see if the values are similar
+		// look for a small dip
+		// see if the values are similar
+		// keep on moving
+		// look for > 7000 i.e. the end of the reflector
+		// rotate to that coordinate - 10 degrees or so
+		// head towards it and stop when you get close
+		// rotate left
+		// follow the wall
+		// rotate left
+		// follow the desk
+		// rotate right
+		// get lit
+	}
 }
